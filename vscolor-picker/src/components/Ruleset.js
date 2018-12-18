94f0a1f2b15522370da0d5b8af8cb62d7287f2e1
Jared E Jackson <jaredjxn15@gmail.com>
2018-12-17 19:35:14 -0500

    Added config file for state

--- vscolor-picker/src/components/Ruleset.js
+++ vscolor-picker/src/components/Ruleset.js
@@ -1,116 +1,31 @@
 import React, { Component } from 'react';
 import './Ruleset.css';
 import Terminal from './Terminal';
+import {workbenchRules, tokenRules} from '../config';
 
 export default class Ruleset extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            "name": "",
-            "type": "dark",
-            "colors": {
-              "activityBar.background": "",
-              "activityBar.foreground": "",
-              "activityBarBadge.background": "",
-              "activityBarBadge.foreground": "",
-              "badge.background": "",
-              "button.background": "",
-              "debugToolBar.background": "",
-              "dropdown.background": "",
-              "diffEditor.insertedTextBackground": "",
-              "dropdown.border": "",
-              "editor.background": "",
-              "editorError.foreground": "",
-              "editorIndentGuide.activeBackground": "",
-              "editorMarkerNavigation.background": "",
-              "editorRuler.foreground": "",
-              "editor.lineHighlightBackground": "",
-              "editor.selectionBackground": "",
-              "editor.selectionHighlightBackground": "",
-              "editor.selectionHighlightBorder": "",
-              "editorCursor.background": "",
-              "editorCursor.foreground": "",
-              "editorBracketMatch.border": "",
-              "editorBracketMatch.background": "",
-              "editor.findMatchBackground": "",
-              "editor.findMatchBorder": "",
-              "editor.findMatchHighlightBackground": "",
-              "editor.wordHighlightBackground": "",
-              "editor.wordHighlightBorder": "",
-              "editor.wordHighlightStrongBackground": "26",
-              "editor.wordHighlightStrongBorder": "",
-              "editorGroup.background": "",
-              "editorGroup.border": "",
-              "editorGroupHeader.tabsBackground": "",
-              "editorIndentGuide.background": "",
-              "editorLineNumber.foreground": "",
-              "editorActiveLineNumber.foreground": "",
-              "editorWhitespace.foreground": "",
-              "editorHoverWidget.background": "",
-              "editorHoverWidget.border": "",
-              "editorSuggestWidget.background": "",
-              "editorSuggestWidget.border": "",
-              "editorSuggestWidget.selectedBackground": "",
-              "editorWidget.background": "",
-              "focusBorder": "",
-              "input.background": "",
-              "list.activeSelectionBackground": "",
-              "list.activeSelectionForeground": "",
-              "list.focusBackground": "",
-              "list.hoverBackground": "",
-              "list.highlightForeground": "",
-              "list.inactiveSelectionBackground": "",
-              "list.inactiveSelectionForeground": "",
-              "peekViewEditor.matchHighlightBackground": "",
-              "scrollbarSlider.background": "",
-              "scrollbarSlider.activeBackground": "",
-              "scrollbarSlider.hoverBackground": "",
-              "sideBar.background": "",
-              "sideBarSectionHeader.background": "",
-              "statusBar.background": "",
-              "statusBar.foreground": "",
-              "statusBarItem.hoverBackground": "",
-              "statusBar.noFolderBackground": "",
-              "statusBar.debuggingBackground": "",
-              "statusBar.debuggingBorder": "",
-              "statusBar.debuggingForeground": "",
-              "tab.activeBackground": "",
-              "tab.border": "",
-              "tab.inactiveBackground": "",
-              "tab.hoverBackground": "",
-              "tab.unfocusedHoverBackground": "",
-              "terminal.foreground": "",
-              "terminal.ansiBlack": "",
-              "terminal.ansiBlue": "",
-              "terminal.ansiGreen": "",
-              "terminal.ansiYellow": "",
-              "titleBar.activeBackground": "",
-              "titleBar.activeForeground": "",
-              "titleBar.inactiveBackground": "",
-              "titleBar.inactiveForeground": ""
-            }
-        }
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleColorChange = this.handleColorChange.bind(this);
+        this.state = {...workbenchRules, ...tokenRules};
+        this.handleWorkbenchColorChange = this.handleWorkbenchColorChange.bind(this);
+        this.handleTokenColorChange = this.handleTokenColorChange.bind(this);
     }
 
-    handleInputChange(event) {
+    handleWorkbenchColorChange(event) {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-    
-        this.setState({
-            [name]: value
-        });
+        const newState = { ...this.state.workbenchColorCustomizations, [name]: value}
+        this.setState({ workbenchColorCustomizations: newState});
     }
 
-    handleColorChange(event) {
+    handleTokenColorChange(event) {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-        const newState = { ...this.state.colors, [name]: value}
-        this.setState({ colors: newState});
+        const newState = { ...this.state.editorTokenColorCustomizations, [name]: value}
+        this.setState({ editorColorCustomizations: newState});
     }
 
     render() {
@@ -118,20 +33,23 @@ export default class Ruleset extends Component {
             <div id="Ruleset-Terminal">
                 <div className="Ruleset">
                     <form className="rule-form">
-                        <label>
-                            Name: <input 
-                            type="text"
-                            name="name"
-                            onChange={this.handleInputChange}/>
-                        </label>
-                        {Object.keys(this.state.colors).map(rule => (
+                        {Object.keys(this.state.workbenchColorCustomizations).map(rule => (
                                 <label key={rule}>
                                 {rule}: <input 
                                     type="color"
                                     name={rule}
-                                    onChange={this.handleColorChange}
-                                    className="label-clr"/> {/* rule is equal to the rule in state*/}
+                                    onChange={this.handleWorkbenchColorChange}
+                                    className={`label-clr`}/>
                                 </label>
+                        ))}
+                        {Object.keys(this.state.editorTokenColorCustomizations).map(rule => (
+                            <label key={rule}>
+                                {rule}: <input 
+                                    type="color"
+                                    name={rule}
+                                    onChange={this.handleTokenColorChange}
+                                    className={`label-clr`}/>
+                            </label>
                         ))}
                     </form>
                 </div>
